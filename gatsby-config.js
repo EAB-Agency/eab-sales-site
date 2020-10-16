d7dddad2a5fa5dcbcad0f705379dcd8d7927a058
Bobby Smith <reversetype@gmail.com>
2020-10-16 11:47:21 -0400

    remove blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,10 @@ require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
-const contentfulConfig = {
-  spaceId: process.env.CONTENTFUL_SPACE_ID,
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-}
+// const contentfulConfig = {
+//   spaceId: process.env.CONTENTFUL_SPACE_ID,
+//   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+// }
 
 const gtmConfig = {
   gtmId: process.env.GTM_ID,
@@ -17,32 +17,28 @@ const { gtmId } = gtmConfig
 // CONTENTFUL_HOST in your environment config
 // the `host` property should map to `preview.contentful.com`
 // https://www.contentful.com/developers/docs/references/content-preview-api/#/reference/spaces/space/get-a-space/console/js
-if (process.env.CONTENTFUL_HOST) {
-  contentfulConfig.host = process.env.CONTENTFUL_HOST
-}
+// if (process.env.CONTENTFUL_HOST) {
+//   contentfulConfig.host = process.env.CONTENTFUL_HOST
+// }
 
-const { spaceId, accessToken } = contentfulConfig
+// const { spaceId, accessToken } = contentfulConfig
 
-if (!spaceId || !accessToken) {
-  throw new Error(
-    'Contentful spaceId and the access token need to be provided.'
-  )
-}
+// if (!spaceId || !accessToken) {
+//   throw new Error(
+//     'Contentful spaceId and the access token need to be provided.'
+//   )
+// }
 
 module.exports = {
   siteMetadata: {
     title: 'College of America',
   },
-  pathPrefix: '/gatsby-contentful-starter',
   plugins: [
-    'gatsby-transformer-remark',
-    'gatsby-transformer-sharp',
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sharp',
-    {
-      resolve: 'gatsby-source-contentful',
-      options: contentfulConfig,
-    },
+    // {
+    //   resolve: 'gatsby-source-contentful',
+    //   options: contentfulConfig,
+    // },
     {
       resolve: 'gatsby-plugin-google-tagmanager',
       options: {
